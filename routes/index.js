5124bacd4ad5593dcedcb9493e4e17097f376bbf
xiunaonao <onlymyalways@163.com>
2018-09-20 19:02:26 +0800

    wechat

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var jsSHA = require('jssha');
 /* GET home page. */
 router.get(['/index','/'], function(req, res, next) {
   res.render('index', { title: '防骚扰平台' });
@@ -13,7 +13,27 @@ router.get('/register',(req,res,next)=>{
 
 
 router.get('/wechat',(req,res,next)=>{
-	res.json({success:true})
+	var token="eastcom_hm";
+	var signature = req.query.signature;
+	var timestamp = req.query.timestamp;
+	var echostr   = req.query.echostr;
+	var nonce     = req.query.nonce;
+
+	var oriArray = new Array();
+	oriArray[0] = nonce;
+	oriArray[1] = timestamp;
+	oriArray[2] = token;
+	oriArray.sort();
+
+	var original = oriArray.join('');
+
+	var shaObj = new jsSHA(original, 'TEXT');
+	var scyptoString=shaObj.getHash('SHA-1', 'HEX'); 
+	if(scyptoString==signature){
+		res.json({success:true})
+	}else{
+		res.json({success:false})
+	}
 })
 
 module.exports = router;
