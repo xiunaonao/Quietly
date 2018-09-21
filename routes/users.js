b5aeda66908f7ba2e5c818ba2caff1de8dbda5f1
xiunaonao <onlymyalways@163.com>
2018-09-21 16:02:01 +0800

    wechat

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user_index',{title:'我的信息'})
+	let tel=req.cookie['t']
+	res.render('user_index',{title:'我的信息',{tel:tel}})
 });
 
 router.get('/note',(req,res,next)=>{
