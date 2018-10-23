791f02160b98f173d4778bff56b881f7d07917ee
xiunaonao <onlymyalways@163.com>
2018-10-23 10:35:54 +0800

    test

--- public/js/note.js
+++ public/js/note.js
@@ -5,6 +5,7 @@ var vapp=new Vue({
 		is_notice:false,
 		noticeid:'',
 		note_list:[],
+		add_type:-1,
 		add_obj:{
 			type:1,
 			tel1:'',
@@ -15,8 +16,57 @@ var vapp=new Vue({
 		}
 	},
 	methods:{
-		move_roster:function(obj){
+		add_roster:function(){
+			var scope=this
+
+			var form={
+					isWished:this.add_type==1?1:0,
+					type:this.add_type==1?5:1,
+					content:'',
+					wantPushNotification:this.add_type==1?0:1
+			}
+
+			switch(this.add_obj.type){
+				case 1:
+					form.content=this.add_obj.tel1+'-'+this.add_obj.tel2;
+					break;
+				case 2:
+					form.content=this.add_obj.tel3;
+					break;
+				case 3:
+					form.content=this.add_obj.tel4;
+					break;
+			}
+
+			if(!this.valid(form.content)){
+				vapp_layer.alert('电话号码不正确')
+				return
+			}
+
+			axios.post('/api/set_setting_type',{form:[form]}).then(function(res){
+				// scope.roster_list.unshift({
+				// 	content:form.content,
+				// 	id:res.data.result.result,
+				// 	createTime:scope.getDateStr(new Date)
+				// });
+
+				scope.add_obj={
+					type:1,
+					tel1:'',
+					tel2:'',
+					tel3:'',
+					tel4:'',
+					remark:''
+				}
+				scope.is_add=false;
+				vapp_layer.alert('已加入'+(scope.add_type==1?'白名单':'黑名单'))
+			})
+			
+
+		},
+		move_roster:function(obj,type){
 			//add_obj=
+			this.add_type=type
 			this.add_obj={
 				type:1,
 				tel1:'',
@@ -25,18 +75,18 @@ var vapp=new Vue({
 				tel4:'',
 				remark:''
 			}
-			if(obj.tel.indexOf('-')>-1){
+			if(obj.interceptNumber.indexOf('-')>-1){
 				this.add_obj.type=1;
-				this.add_obj.tel1=obj.tel.split('-')[0];
-				this.add_obj.tel2=obj.tel.split('-')[1];
-			}else if(obj.tel.length==11 && obj.tel[0]=='1'){
+				this.add_obj.tel1=obj.interceptNumber.split('-')[0];
+				this.add_obj.tel2=obj.interceptNumber.split('-')[1];
+			}else if(obj.interceptNumber.length==11 && obj.interceptNumber[0]=='1'){
 				this.add_obj.type=2;
-				this.add_obj.tel3=obj.tel;
+				this.add_obj.tel3=obj.interceptNumber;
 			}else {
 				this.add_obj.type=3;
-				this.add_obj.tel4=obj.tel;
+				this.add_obj.tel4=obj.interceptNumber;
 			}
-			this.add_obj.remark=obj.remark;
+			this.add_obj.remark=obj.tag;
 			this.is_add=true;
 
 		},
@@ -78,52 +128,70 @@ var vapp=new Vue({
 
 			})
 		},
-		add_roster:function(){
-			var scope=this
+		valid:function(tel){
+			let isok=false
+			let err=''
 
-			var form={
-					isWished:roster_type==1?1:0,
-					type:roster_type==1?5:1,
-					content:'',
-					wantPushNotification:roster_type==1?0:1
+			if(this.add_obj.type==2 && tel.match(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/)){
+				isok=true
+			}else{
 			}
 
-			switch(this.add_obj.type){
-				case 1:
-					form.content=this.add_obj.tel1+'-'+this.add_obj.tel2;
-					break;
-				case 2:
-					form.content=this.add_obj.tel3;
-					break;
-				case 3:
-					form.content=this.add_obj.tel4;
-					break;
+			if(this.add_obj.type==1 && tel.match(/^0\d{2,3}-\d{7,8}$/)){
+				isok=true
+			}else{
+
 			}
 
-			if(!this.valid(form.content)){
-				vapp_layer.alert('电话号码不正确')
-				return
-			}
+			return isok
 
-			axios.post('/api/set_setting_type',{form:[form]}).then(function(res){
-				scope.roster_list.unshift({
-					content:form.content,
-					id:res.data.result.result,
-					createTime:scope.getDateStr(new Date)
-				});
-
-				scope.add_obj={
-					type:1,
-					tel1:'',
-					tel2:'',
-					tel3:'',
-					tel4:'',
-					remark:''
-				}
-				scope.is_add=false;
-				vapp_layer.alert('已加入'+(roster_type==1?'白名单':'黑名单'))
-			})
 		}
+		// add_roster:function(){
+		// 	var scope=this
+
+		// 	var form={
+		// 			isWished:roster_type==1?1:0,
+		// 			type:roster_type==1?5:1,
+		// 			content:'',
+		// 			wantPushNotification:roster_type==1?0:1
+		// 	}
+
+		// 	switch(this.add_obj.type){
+		// 		case 1:
+		// 			form.content=this.add_obj.tel1+'-'+this.add_obj.tel2;
+		// 			break;
+		// 		case 2:
+		// 			form.content=this.add_obj.tel3;
+		// 			break;
+		// 		case 3:
+		// 			form.content=this.add_obj.tel4;
+		// 			break;
+		// 	}
+
+		// 	if(!this.valid(form.content)){
+		// 		vapp_layer.alert('电话号码不正确')
+		// 		return
+		// 	}
+
+		// 	axios.post('/api/set_setting_type',{form:[form]}).then(function(res){
+		// 		scope.roster_list.unshift({
+		// 			content:form.content,
+		// 			id:res.data.result.result,
+		// 			createTime:scope.getDateStr(new Date)
+		// 		});
+
+		// 		scope.add_obj={
+		// 			type:1,
+		// 			tel1:'',
+		// 			tel2:'',
+		// 			tel3:'',
+		// 			tel4:'',
+		// 			remark:''
+		// 		}
+		// 		scope.is_add=false;
+		// 		vapp_layer.alert('已加入'+(roster_type==1?'白名单':'黑名单'))
+		// 	})
+		// }
 	},
 	mounted:function(){
 		//this.note_list=[];
